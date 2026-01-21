01a9c7b4ca600a8b915e02e884257eb552cb4c28
Belal Ibrahim <belal.ibrahim@proton.me>
2026-01-21 18:14:29 +0100

    (WIP) feature/gh-7-drupal
    - WIP: Start setting up Drupal, but it looks like it might be a little
      tricky...

--- lib/config.sh
+++ lib/config.sh
@@ -22,11 +22,13 @@ declare -A APP_PORT_MAPPING
 APP_IMAGE[wordpress]="wordpress:latest"
 APP_IMAGE[joomla]="joomla:latest"
 APP_IMAGE[redmine]="redmine:latest"
+APP_IMAGE[drupal]="drupal:latest"
 
 # Database type
 APP_DB[wordpress]="mysql"
 APP_DB[joomla]="mysql"
 APP_DB[redmine]="mysql"
+APP_DB[drupal]="mysql"
 
 # Env var mappings
 APP_DB_HOST_VAR[wordpress]="WORDPRESS_DB_HOST"
@@ -44,12 +46,23 @@ APP_DB_NAME_VAR[redmine]="REDMINE_DB_DATABASE"
 APP_DB_USER_VAR[redmine]="REDMINE_DB_USERNAME"
 APP_DB_PASS_VAR[redmine]="REDMINE_DB_PASSWORD"
 
+# TODO: Drupal doesn't support these?
+# So how to do it... omit conditionally in setup_db() (see db.sh)?
+APP_DB_HOST_VAR[drupal]=""
+APP_DB_NAME_VAR[drupal]=""
+APP_DB_USER_VAR[drupal]=""
+APP_DB_PASS_VAR[drupal]=""
+
 # Volumes (sometimes required)
 APP_VOLUME[wordpress]="/var/www/html/wp-content"
 APP_VOLUME[joomla]=""
 APP_VOLUME[redmine]=""
+# TODO: Apparently, Drupal might need several.
+# But for now, let's just see if it works without.
+APP_VOLUME[drupal]=""
 
 # Port mappings (sometimes required)
 APP_PORT_MAPPING[wordpress]=""
 APP_PORT_MAPPING[joomla]=""
 APP_PORT_MAPPING[redmine]="3000"
+APP_PORT_MAPPING[drupal]=""
