39f8b8e514ba26dda5c7e3e5889a09ac62ba09bc
Belal Ibrahim <belal.ibrahim@proton.me>
2026-01-25 12:46:18 +0100

    (WIP) feature/gh-11-odoo
    - WIP: Add support for Odoo. Not working, database connection issue...

--- lib/config.sh
+++ lib/config.sh
@@ -17,6 +17,7 @@ declare -A APP_PORT_MAPPING
 APP_DB[wordpress]="mysql"
 APP_DB[joomla]="mysql"
 APP_DB[redmine]="mysql"
+APP_DB[odoo]="postgres"
 
 # Env var mappings
 APP_DB_HOST_VAR[wordpress]="WORDPRESS_DB_HOST"
@@ -34,12 +35,19 @@ APP_DB_NAME_VAR[redmine]="REDMINE_DB_DATABASE"
 APP_DB_USER_VAR[redmine]="REDMINE_DB_USERNAME"
 APP_DB_PASS_VAR[redmine]="REDMINE_DB_PASSWORD"
 
+APP_DB_HOST_VAR[odoo]="HOST"
+APP_DB_NAME_VAR[odoo]="NAME"
+APP_DB_USER_VAR[odoo]="USER"
+APP_DB_PASS_VAR[odoo]="PASSWORD"
+
 # Volumes (sometimes required)
 APP_VOLUME[wordpress]="/var/www/html/wp-content"
 APP_VOLUME[joomla]=""
 APP_VOLUME[redmine]=""
+APP_VOLUME[odoo]=""
 
 # Port mappings (sometimes required)
 APP_PORT_MAPPING[wordpress]=""
 APP_PORT_MAPPING[joomla]=""
 APP_PORT_MAPPING[redmine]="3000"
+APP_PORT_MAPPING[odoo]="8069"
